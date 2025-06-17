894dd1f924b46b700f131ce5719c5400797d7d08
Vova Kondysiuk <vovakondysiuk20@gmail.com>
2025-06-17 14:34:10 +0300

    implemented the RobotRoute method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,35 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int deltaX = toX - robot.getX();
+        int deltaY = toY - robot.getY();
+        if (deltaX > 0) {
+            rotateToDirection(robot, Direction.RIGHT);
+            for (int i = 0; i < deltaX; i++) {
+                robot.stepForward();
+            }
+        } else if (deltaX < 0) {
+            rotateToDirection(robot, Direction.LEFT);
+            for (int i = 0; i < -deltaX; i++) {
+                robot.stepForward();
+            }
+        }
+        if (deltaY > 0) {
+            rotateToDirection(robot, Direction.UP);
+            for (int i = 0; i < deltaY; i++) {
+                robot.stepForward();
+            }
+        } else if (deltaY < 0) {
+            rotateToDirection(robot, Direction.DOWN);
+            for (int i = 0; i < -deltaY; i++) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    private static void rotateToDirection(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnRight();
+        }
     }
 }
